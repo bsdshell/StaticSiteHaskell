748ae1276f9fa0864df0b66a42490c8d91513088
bsdshell <monosurface@gmail.com>
2015-12-25 16:52:26 -0800

    just do it

---
+++ geneHtml.hs
@@ -0,0 +1,181 @@
+import System.IO
+import System.Environment
+import Text.Regex.Posix
+import Text.Regex
+import Data.List 
+import Data.List.Split 
+
+-- split into two lists: odd index list and even index list
+splitList::[String]->([String], [String])
+splitList [] = ([], [])
+splitList [x] = ([x], [])
+splitList (x:y:xs) = (x:xp, y:yp) where (xp, yp) = splitList xs
+
+unwrap::Maybe a -> a
+unwrap Nothing = error "this is nothing from unwrap" 
+unwrap (Just x) = x
+
+codelist = splitRegex(mkRegex "\\[|\\]") "what[code1][code2]"
+
+
+
+style::String->String->String->[String]->[String]
+style pat l r list = map(`op` rep) list 
+                        where op = subRegex $ mkRegex pat 
+                              rep = l ++ match ++ r
+                              match = "\\0"
+
+replace::String->String->[String]->[String]
+replace pat rep list = map (`op` rep) list
+                        where op = subRegex $ mkRegex pat
+
+htmlTab::String->String->[String]->[String]
+htmlTab _ _ [] = []
+htmlTab left right list = map(left ++) $ map (++ right) list
+
+html::String->String->[String]->String
+html open close list = open ++ (unlines list) ++ close
+
+codeHighLight::String->[String]->[String]
+codeHighLight pat list = map(`op` rep) list 
+                where rep = "<span class=\"bracket\">\\1</span>"
+                      op = subRegex $ mkRegex pat
+
+codeCapture::String->String
+codeCapture str = subRegex(mkRegex pat) str rep 
+                    where rep = "<cool>\\1</cool>" 
+                          pat = "(([^`]|`[^[]]*|\n*)*)"
+
+codeBlock::String->String->String->String
+codeBlock open close str = op str rep
+                        where op = subRegex (mkRegex pat)  
+                              rep = open ++ "\\1" ++ close
+                              pat = codeOpen ++ "(([^`]|`[^[]]*|\n*)*)" ++ codeClose --fix it
+                                  where 
+                                        codeOpen = "^[[:space:]]*`\\[[[:space:]]*$"
+                                        codeClose = "[[:space:]]*`\\][[:space:]]*$"
+
+mergeList::[String]->[String]->[String]
+mergeList [] [] = []
+mergeList  l  [] = l 
+mergeList []  r = r 
+mergeList (x:xs) (y:ys) = x:y:mergeList xs ys 
+
+mergeList2::[a]->[a]->Maybe [a]
+mergeList2 [] [] = Just [] 
+mergeList2 (x:xs) (y:ys) = 
+            case mergeList2 xs ys of
+            Just merged -> Just (x:y:merged)
+            Nothing  -> Nothing
+mergeList2 _ _  = Nothing
+
+--codeBlock::String->String->String->String
+--codeBlock open close str = op str rep
+--                        where op = subRegex (mkRegex pat)  
+--                              --rep = open ++ "\\1" ++ close
+--                              rep = open ++ newrep ++ close
+--                              --newrep = subRegex(mkRegex "(\\[)") mstr  "<span class=\"bracket\">\1</span>" 
+--                              --newrep = subRegex(mkRegex "(\\[)") ("aron\1aron2"++mstr) mstr 
+--                              newrep = subRegex(mkRegex "return")  mstr "aron\\1aron\\0aron\0aronaron" 
+--                              where mstr = "\\1"
+--                                    pat = codeOpen ++ "(([^`]|`[^[]]*|\n*)*)" ++ codeClose --fix it
+--                                        where 
+--                                              codeOpen = "^[[:space:]]*`\\[[[:space:]]*$"
+--                                              codeClose = "[[:space:]]*`\\][[:space:]]*$"
+
+lt                      =  "(<)"
+gt                      =  "(>)"
+
+html_lt                 =  "&lt;"
+html_gt                 =  "&gt;"
+
+htmlOpen                =  "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"mycss/style.css\"/></head><body class=\"htmlbg\">"
+htmlClose               =  "</body></html>"
+
+openSpan                =  "<span class=\"keyword\">"
+closeSpan               =  "</span>"
+
+-- //xxx, // xxx
+comment                 =  "//.*$"
+commentOpen             =  "<span class=\"comment\">"
+commentClose            =  "</span>"
+
+-- :xxx
+keyword                 =  "^[[:space:]]*(:[[:graph:]]+)[[:space:]]*"
+
+-- [ xxx ], [xxx]
+titleOpen               =  "<span class=\"tit\">"
+titleClose              =  "</span>"
+title                   =  "^[[:space:]]*\\[[a-zA-Z0-9 ]+\\]"
+
+-- { xxx }, {xxx}
+header                  =  "^[[:space:]]*{[a-zA-Z0-9 ]+}"                                                                       
+headerOpen              =  "<span class =\"header\">"
+headerClose             =  "</span>"
+preOpen                 =  "<pre>"
+preClose                =  "</pre>"
+
+-- 1. , 2.
+numName                 =  "^[[:space:]]*[0-9]+\\.[[:space:]]+"
+spanNumOpen             =  "<span class=\"num\">"
+spanNumCose             =  "</span>"
+
+html_tab                =  "^[[:space:]]*(<[^<>]+>[^<>]*<[[:space:]]*/[^<>]+>.*$)"
+
+pattern                 = "(\\[|\\]|\\(|\\)|{|}|git|maybe |-&gt;)"
+pattern1                = "([[:graph:]]+::)"
+--pattern                 = "(git)"
+
+main = do
+    print("geneHtml input.txt out.txt")
+    [inFile, outFile] <- getArgs
+    handle <- openFile inFile ReadMode
+    contents <- hGetContents handle
+
+    let line      = lines contents
+
+    let r_title   = mkRegex title
+    let r_header  = mkRegex header
+    let r_comment = mkRegex comment
+
+    let list0     = replace lt html_lt line
+    let list1     = replace gt html_gt list0 
+    let list2     = style keyword openSpan closeSpan list1 
+    let list3     = style title titleOpen titleClose list2 
+    let list4     = style comment commentOpen commentClose list3
+    let list5     = style header headerOpen headerClose list4
+    let list6     = style numName spanNumOpen spanNumCose list5
+    let list7     = replace html_tab "\\0<br>" list6
+    let list8     = codeBlock preOpen preClose (unlines list7):[]
+    print $ codeCapture "`[ what is new `]" 
+    --print $ filter(\x-> (isInfixOf "`[" x) || (isInfixOf "`]" x) ) $ splitRegex(mkRegex " |\n") (unlines list6)
+    --let splitcode  = splitRegex(mkRegex "[[:space:]]+`\\[[[:space:]]+|[[:space:]]+`\\][[:space:]]+") (unlines list7)
+    ---let splitcode  = splitRegex(mkRegex "([[:blank:]\n]+`\\[[[:blank:]]*\n)|([[:blank:]\n]+`\\][[:blank:]]*\n)") (unlines list7)
+    let splitcode  = splitRegex(mkRegex "([[:blank:]]+`\\[[[:blank:]]*\n)|([[:blank:]]+`\\][[:blank:]]*\n)") (unlines list7)
+
+    let oddList    = fst $ splitList splitcode
+    let evenList   = snd $ splitList splitcode
+    print evenList
+
+    --let pat        = "(\\[|\\]|\\(|\\)|{|})"
+    let styleCode1         = codeHighLight pattern evenList 
+    let styleCode         = codeHighLight pattern1 styleCode1 
+    let finalStyleCode     = map(preOpen ++) $ map(++ preClose) styleCode 
+    print finalStyleCode
+    
+    print "----------------------------------------"
+    print styleCode 
+    print "----------------------------------------"
+    print finalStyleCode
+    print "----------------------------------------"
+    print $ length oddList
+    print $ length styleCode  
+
+    let mergeAllList = mergeList oddList finalStyleCode 
+    print "[================================================="
+    print mergeAllList 
+    print "]================================================="
+    writeFile outFile $ html htmlOpen htmlClose mergeAllList 
+
+    --putStr contents
+    hClose handle
